92a9f2a6e5b2d280ea4add2c2595fcbfd767cf40
Semih <semih9569@gmail.com>
2023-05-18 19:34:33 +0300

    94. Binary Tree Inorder Traversal

---
+++ 94. Binary Tree Inorder Traversal/inorderTraversal.ts
@@ -0,0 +1,24 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function inorderTraversal(root: TreeNode | null): number[] {
+  const result: number[] = [];
+  if (root === null) return result;
+
+  const traverseOrder = (node: TreeNode) => {
+    node.left && traverseOrder(node.left);
+    result.push(node.val);
+    node.right && traverseOrder(node.right);
+  };
+  traverseOrder(root);
+
+  return result;
+}
